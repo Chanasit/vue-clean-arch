c03fb2fabeda2aa95b4a636a0099e371d92ff3d8
Chanasit <kai.chr160@gmail.com>
2020-12-07 14:29:11 +0700

    fix wrong interface type

--- src/repositories/httpclients/Application.ts
+++ src/repositories/httpclients/Application.ts
@@ -4,14 +4,14 @@ import {ajax} from 'rxjs/ajax'
 import {GetGithubUserProfileDAO, GetGithubUserProfileAdapter} from './adapters/GetGithubUserProfile'
 
 export interface IAppHttpClient {
-	getGithubUserProfile(): Observable<Array<GetGithubUserProfileDAO>>
+	getGithubUserProfile(): Observable<GetGithubUserProfileDAO[]>
 }
 
 export class AppHttpClient implements IAppHttpClient {
 
 	constructor() {}
 
-	getGithubUserProfile(): Observable<Array<GetGithubUserProfileDAO>> {
+	getGithubUserProfile(): Observable<GetGithubUserProfileDAO[]> {
 		return ajax(`https://api.github.com/users?per_page=10`).pipe(map(r => new GetGithubUserProfileAdapter().adapt(r.response)))
 	}
 
--- src/services/Application.ts
+++ src/services/Application.ts
@@ -1,16 +1,17 @@
-import {Observable} from 'rxjs';
-import {IAppHttpClient} from '../repositories/httpclients/Application'
+import { Observable } from 'rxjs';
+import { GetGithubUserProfileDAO } from '../repositories/httpclients/adapters/GetGithubUserProfile';
+import { IAppHttpClient } from '../repositories/httpclients/Application'
 
 export interface IAppService {
-	getGithubUserProfile(): Observable<string>
+    getGithubUserProfile(): Observable<GetGithubUserProfileDAO[]>
 }
 
 export class AppService implements IAppService {
-	constructor(
-		private appHttpClient: IAppHttpClient
-	) {}
+    constructor(
+        private appHttpClient: IAppHttpClient
+    ) { }
 
-	getGithubUserProfile(): Observable<string> {
-		return this.appHttpClient.getGithubUserProfile()
-	}
+    getGithubUserProfile(): Observable<GetGithubUserProfileDAO[]> {
+        return this.appHttpClient.getGithubUserProfile()
+    }
 }
